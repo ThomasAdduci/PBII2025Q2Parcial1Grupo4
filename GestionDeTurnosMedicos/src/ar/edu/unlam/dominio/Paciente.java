3486d77f82318d4baf8ac32e1922ab44db1dd18e
Micaela <micamazza@hotmail.com>
2025-10-01 15:46:38 -0300

    Agregue algunas clases principales y test simples

---
+++ GestionDeTurnosMedicos/src/ar/edu/unlam/dominio/Paciente.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.dominio;
+
+public class Paciente extends Persona {
+
+	public Paciente(String nombre, String apellido, Integer edad, Integer dni) {
+		super(nombre, apellido, edad, dni);
+	}
+
+}
---
+++ GestionDeTurnosMedicos/src/ar/edu/unlam/dominio/Persona.java
@@ -0,0 +1,58 @@
+package ar.edu.unlam.dominio;
+
+import java.util.Objects;
+
+public abstract class Persona {
+
+	private String nombre;
+	private String apellido;
+	private Integer edad;
+	private Integer dni;
+	
+	
+	public Persona(String nombre, String apellido, Integer edad, Integer dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.dni = dni;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+
+	public Integer getDni() {
+		return dni;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(dni, other.dni);
+	}
+	
+	
+	
+	
+	
+}
